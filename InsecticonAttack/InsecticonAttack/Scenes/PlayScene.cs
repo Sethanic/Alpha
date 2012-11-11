99b157fe864661ea5bec0dcaae5b8e5b4ae05378
Russ Painter <russp@geekymonkey.com>
2012-11-11 22:21:48 +0000

    Added a few things to get you started
    * 2 backgrounds
    * A font
    * A sprite (start button) that changes color when the mouse is over it

--- InsecticonAttack/InsecticonAttack/Scenes/PlayScene.cs
+++ InsecticonAttack/InsecticonAttack/Scenes/PlayScene.cs
@@ -5,12 +5,33 @@ using System.Text;
 
 namespace ScratchyXna
 {
+    /// <summary>
+    /// The Play Scene
+    /// </summary>
     class PlayScene : Scene
     {
+        /// <summary>
+        /// Load happens once when the scene is created
+        /// </summary>
         public override void Load()
         {
-            
+            // Background
+            Background.AddLayer("PlaySceneBackground");
+            Background.ScaleToScreen();
+
+            // Text
+            LoadFont("QuartzMS");
+            Text titleText = new Text
+            {
+                Value = "This is the play scene"
+            };
+            AddText(titleText);  // Adds the title text object to the scene, so that it will be drawn
         }
+
+        /// <summary>
+        /// Update happens 30-60 times a second
+        /// </summary>
+        /// <param name="gameTime">The amount of time since the last update</param>
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
             
--- InsecticonAttack/InsecticonAttack/Scenes/titleScene.cs
+++ InsecticonAttack/InsecticonAttack/Scenes/titleScene.cs
@@ -5,15 +5,49 @@ using System.Text;
 
 namespace ScratchyXna
 {
+    /// <summary>
+    /// The Title Scene
+    /// </summary>
     class TitleScene : Scene
     {
+        StartButton startButton;
+
+        /// <summary>
+        /// Load happens once when the scene is created
+        /// </summary>
         public override void Load()
         {
-            
+            // Background
+            Background.AddLayer("TitleSceneBackground");
+            Background.ScaleToScreen();
+
+            // Text
+            LoadFont("QuartzMS");
+            Text titleText = new Text
+            {
+                Value = "This is the title scene",
+                VerticalAlign = VerticalAlignments.Top,
+                Alignment = HorizontalAlignments.Center,
+                X = 0,
+                Y = 90
+            };
+            AddText(titleText);  // Adds the title text object to the scene, so that it will be drawn
+
+            // Sprites
+            startButton = new StartButton();
+            AddSprite(startButton);
         }
+
+        /// <summary>
+        /// Update happens 30-60 times a second
+        /// </summary>
+        /// <param name="gameTime">The amount of time since the last update</param>
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            
+            if (Mouse.Button1Down() && startButton.IsTouching(Mouse.Position))
+            {
+                ShowScene("Play");
+            }
         }
     }
 }
