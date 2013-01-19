5d6a4ca553c7524fd73340d97c2f80e103149de3
Cian Perill <bperill16@gmail.com>
2013-01-19 12:35:51 +0000

    Incomplete MetalLevelHUD

---
+++ InsecticonAttack/InsecticonAttack/Sprites/MetalLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScratchyXna.Sprites
+{
+    class MetalLevel : Sprite
+    {
+        public override void Load()
+        {
+            AddCostume("MetalLevel");
+            //SetSize
+        }   
+    }
+}
